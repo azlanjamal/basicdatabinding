556e4fc2c3aeeacf3a35fe032765fc25b7966101
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-03 13:49:51 +0800

    Set content view with data binding. Get data and bind it to layout.

--- app/src/main/java/com/azlanjamal/basicdatabinding/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicdatabinding/MainActivity.java
@@ -1,13 +1,24 @@
 package com.azlanjamal.basicdatabinding;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.azlanjamal.basicdatabinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Inflate layout
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        // Get data
+        DataSource dataSource = DataSource.get("Azlan");
+
+        // Bind the data to the view
+        binding.setDataSource(dataSource);
     }
 }
